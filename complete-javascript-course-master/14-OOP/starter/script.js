c659799fcf015b353d5864f7c9df8f01a1ef15c5
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-05-19 17:00:09 +0100

    add the function in the prototype

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -3,7 +3,10 @@
 const Person = function (firstName, birthYear) {
     this.firstName = firstName;
     this.birthYear = birthYear;
-    
+   // never do this becouse it will create a new function every time
+/*     this.calcAge = function () {
+        console.log(2025 - this.birthYear);
+    } */
 }
 
 const jonas=new Person('Jonas', 1991);
@@ -12,6 +15,7 @@ const jonas=new Person('Jonas', 1991);
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically returns {}
+console.log("hicham");
 
 
 console.log(jonas);
@@ -24,5 +28,23 @@ console.log(matilda, jack);
 console.log(jonas instanceof Person); // true
 
 
+// Prototypes
+Person.prototype.calcAge = function () {
+    console.log(2025 - this.birthYear);
+}
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
 
  
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
