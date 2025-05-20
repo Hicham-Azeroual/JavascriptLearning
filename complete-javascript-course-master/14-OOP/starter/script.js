4a0e5572bfa4e145e1370eafb6112880188148b9
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-05-20 10:04:49 +0100

    inhiretence in es6 classes

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -102,3 +102,101 @@ bmw.accelerate();
 mercedes.accelerate();
 bmw.brake();
 mercedes.brake();
+
+
+// ES6 Classes
+// class expression
+/* const PersonCle = class {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+}
+calcAge() {
+    console.log(2037 - this.birthYear);
+}
+
+} */
+
+//class declaration
+class PersonCl {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    }
+    get fullName() {
+        return this._fullName;
+    }
+    set fullName(name) {
+        if (name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+    static hello() {
+        console.log('Hey there 👋');
+    }
+
+
+}
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+PersonCl.hello();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+    
+    console.log(`Hey ${this.fullName}`);
+}
+
+jessica.greet();
+
+// 1. Classes are not hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
+
+// static method
+PersonCl.hey = function () {
+    console.log('Hey there 👋');
+    console.log(this);
+}
+PersonCl.hey();
+// const jessica = new PersonCl('Jessica Davis', 1996);
+// jessica.hey(); // undefined 
+
+
+// Object.create
+
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+}
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge(); 
+
+class Student extends PersonCl {
+    constructor(fullName, birthYear, course) {
+        // Always needs to happen first!
+        super(fullName, birthYear);
+        this.course = course;
+    }
+    introduce() {
+        console.log(`My name is ${this.fullName} and I study ${this.course}`);
+    }
+    calcAge() {
+        console.log(`I'm ${2037 - this.birthYear} years old, but as a student I feel more like ${2037 - this.birthYear + 10}`);
+    }
+}
+
